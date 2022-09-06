4f439817fd5ab22c41440ae633c530033545fffe
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2022-09-06 13:11:11 +0300

    countryCodes

--- src/components/CurrencySelect.vue
+++ src/components/CurrencySelect.vue
@@ -47,33 +47,47 @@ export default {
             }
         },
         getLocationApp(){
-            var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/country";
-            var token = "c5190f2f47c35ecf0f6b5498d3a1d1433a930ccc";
-            var query = "ru";
+            // var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/country";
+            // var token = "c5190f2f47c35ecf0f6b5498d3a1d1433a930ccc";
+            // var query = "ru";
 
-            var options = {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": "Token " + token
-                },
-                body: JSON.stringify({query: query})
-            }
+            // var options = {
+            //     method: "POST",
+            //     mode: "cors",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //         "Accept": "application/json",
+            //         "Authorization": "Token " + token
+            //     },
+            //     body: JSON.stringify({query: query})
+            // }
 
-            fetch(url, options)
-            .then(response => response.text())
-            .then(result =>{
-                console.log(JSON.parse(result).suggestions[0].data['alfa2'])
-                this.country = JSON.parse(result).suggestions[0].data['alfa2']
+            // fetch(url, options)
+            // .then(response => response.text())
+            // .then(result =>{
+            //     console.log(JSON.parse(result).suggestions[0].data['alfa2'])
+            //     this.country = JSON.parse(result).suggestions[0].data['alfa2']
+            //     if(this.codesList.hasOwnProperty(this.country)){
+            //         this.currentCurrency = this.codesList[this.country]
+            //         this.$emit('changeCurrency', this.currentCurrency)
+            //     }
+            // } 
+            // )
+            // .catch(error => console.log("error", error));
+
+
+            fetch("https://ipinfo.io/json?token=6e2d6608a3547e").then(
+            (response) => response.json()
+            ).then(
+            (jsonResponse) => {
+                console.log(jsonResponse.ip, jsonResponse.country)
+                this.country = jsonResponse.country
                 if(this.codesList.hasOwnProperty(this.country)){
                     this.currentCurrency = this.codesList[this.country]
                     this.$emit('changeCurrency', this.currentCurrency)
                 }
-            } 
+            }
             )
-            .catch(error => console.log("error", error));
         },
         },
     mounted(){
